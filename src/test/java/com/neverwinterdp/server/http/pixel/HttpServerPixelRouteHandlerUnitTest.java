e408d20c6e72b7deab067269040cb6436af99789
KingOfPoptart <richard.c.duarte@gmail.com>
2014-08-29 11:18:51 +0100

    Moving pixelroute to sparkngin

--- src/test/java/com/neverwinterdp/server/http/pixel/HttpServerPixelRouteHandlerUnitTest.java
+++ src/test/java/com/neverwinterdp/server/http/pixel/HttpServerPixelRouteHandlerUnitTest.java
@@ -1,52 +1,55 @@
-package com.neverwinterdp.sparkngin.http;
+package com.neverwinterdp.server.http.pixel;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpResponse;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.neverwinterdp.netty.http.PixelRouteHandler;
+import com.neverwinterdp.netty.http.HttpServer;
 import com.neverwinterdp.netty.http.client.AsyncHttpClient;
 import com.neverwinterdp.netty.http.client.ResponseHandler;
 import com.neverwinterdp.server.Server;
-import com.neverwinterdp.server.shell.Shell;
-
+import com.neverwinterdp.server.gateway.ClusterGateway;
+import com.neverwinterdp.server.http.pixel.PixelRouteHandler;
 /**
  * @author Richard Duarte
  */
-public class SparknginHttpPixelServerUnitTest {
-  public Server sparknginServer;
-  public Shell shell;
-  public int port=8181;
+public class HttpServerPixelRouteHandlerUnitTest {
   static {
+    System.setProperty("app.dir", "build/cluster") ;
     System.setProperty("log4j.configuration", "file:src/main/resources/log4j.properties") ;
   }
   
-  @Before
-  public void setup() throws Exception {
-    sparknginServer = Server.create("-Pserver.name=sparkngin", "-Pserver.roles=sparkngin") ;
-    
-    shell = new Shell() ;
-    shell.getShellContext().connect();
-    shell.execute(
-        "module install" +
-        "  --member-role sparkngin" +
-        "  --autostart --module Sparkngin" +
-        "  -Pmodule.data.drop=true" +
-        "  -Psparkngin:http-listen-port="+Integer.toString(port) +
-        "  -Psparkngin:forwarder-class=" + NullDevMessageForwarder.class.getName() +
-        "  -Psparkngin:pixelServer=true"
+  static protected Server   instance ;
+  static ClusterGateway gateway ;
+  static int port = 8080;
+  
+  @BeforeClass
+  static public void setup() throws Exception {
+    String[] args = {
+      "-Pserver.group=NeverwinterDP", "-Pserver.name=webserver", "-Pserver.roles=webserver"
+    };
+
+    instance = Server.create(args) ;
+    gateway = new ClusterGateway() ;
+    gateway.execute(
+        "module install " +
+        " -Phttp:port="+Integer.toString(port) +
+        " -Phttp:route.names=pixel" +
+        " -Phttp:route.pixel.handler=com.neverwinterdp.server.http.pixel.PixelRouteHandler" +
+        " -Phttp:route.pixel.path=/pixel" +
+        " --member-name webserver --autostart --module Http"
     ) ;
   }
-  
-  @After
-  public void teardown() {
-    sparknginServer.shutdown() ;
+
+  @AfterClass
+  static public void teardown() throws Exception {
+    gateway.close(); 
+    instance.exit(0) ;
   }
   
   @Test
@@ -64,7 +67,7 @@ public class SparknginHttpPixelServerUnitTest {
     
     //Make sure no failure was caught since ResponseHandler doesn't have error handling
     //and it eats up the error without reporting a test failure
-    assertFalse(handler.getFailure());
+    assertEquals(0,handler.getFailure());
     
     client.close();
   }
@@ -74,8 +77,8 @@ public class SparknginHttpPixelServerUnitTest {
    * it matches the content served from PixelRouteHandler
    */
   static public class PixelCheckResponseHandler implements ResponseHandler {
-    int count=0; 
-    boolean failure = false;
+    int count = 0; 
+    int failure = 0;
     
     public void onResponse(HttpResponse response) {
       count++;
@@ -85,7 +88,7 @@ public class SparknginHttpPixelServerUnitTest {
         assertEquals(PixelRouteHandler.getImageBytes(), buf);
       }
       catch(AssertionError e){
-        failure = true;
+        failure++;
         e.printStackTrace();
       }
     }
@@ -94,8 +97,7 @@ public class SparknginHttpPixelServerUnitTest {
       return this.count;
     }
     
-    //Adding this because there's no error handling in ResponseHandler
-    public boolean getFailure(){
+    public int getFailure(){
       return this.failure;
     }
     
